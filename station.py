bf23e7ab455b49a0292c73f4acc62488100c6b5e
VARUN TOTAKURA <totakura.varun@gmail.com>
2023-11-26 16:59:59 -0500

    Update

--- station.py
+++ station.py
@@ -9,6 +9,7 @@ import select
 import threading
 import time
 from queue import Queue
+from utils import *
 
 HEADER_LENGTH = 1024
 
@@ -309,40 +310,110 @@ class Station:
     def close(self):
         self.client_socket.close()
 
-def load_from_json_file(file_name):
-    try:
-        with open(file_name, 'r') as f:
-            # For interface and hostname files
-            if 'interface' in file_name or 'hostname' in file_name:
-                # Assuming tab-separated values
-                data = [line.strip().split('\t') for line in f]
+class Router(Station):
+    def __init__(self, ip_address, mac_address):
+        super().__init__(ip_address, mac_address)
+        self.connected_bridges = {}
+        self.host_name = load_from_json_file('hostname.json')
+        self.routing_table = load_from_json_file('routingtable.json')
+        self.interface_info = load_from_json_file('interface.json')
 
-                # Create a dictionary
-                all_data = {row[0]: row[1:] for row in data}
+    def connect_to_bridges(self):
+        for interface, bridge_info in self.interface_info.items():
+            bridge_name, bridge_port = bridge_info.split()
+            ip_address = self.hostname_mapping.get(bridge_name)
+            if ip_address:
+                try:
+                    # Initialize a TCP socket connection to the bridge
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                        self.set_socket_nonblocking(s)
+                        s.connect((ip_address, int(bridge_port)))
+                        retries = 5
+                        wait_time = 2  # seconds
+                        for _ in range(retries):
+                            ready_to_read, _, _ = select.select([s], [], [], wait_time)
+                            if ready_to_read:
+                                response = s.recv(HEADER_LENGTH).decode('utf-8')
+                                if response == 'accept':
+                                    self.connected_bridges[interface] = s
+                                    print(f"Connected to {bridge_name} on interface {interface}")
+                                    break
+                                else:
+                                    print(f"Connection to {bridge_name} on interface {interface} rejected")
+                                    break
+                            else:
+                                print(f"Retrying connection to {bridge_name} on interface {interface}")
+                        else:
+                            print(f"Failed to connect to {bridge_name} on interface {interface}")
+                except Exception as e:
+                    print(f"Error connecting to {bridge_name}: {e}")
 
-            # For routing table file
-            elif 'routingtable' in file_name:
-                # Assuming space-separated values
-                data = [line.strip().split() for line in f]
-
-                # Create a list of dictionaries
-                all_data = [{'destination': row[0], 'next_hop': row[1], 'subnet_mask': row[2], 'interface': row[3]} for row in data]
-
+    def forward_packet(self, destination_ip, message):
+        # Decide which interface to use based on the routing table
+        interface = self.get_routing_interface(destination_ip)
+        if interface:
+            destination_mac = self.arp_table.get_mac_address(destination_ip)
+            if destination_mac:
+                frame = create_frame(self.mac_address, destination_mac, message)
+                self.connected_bridges[interface].send(frame)
             else:
-                # Unsupported file type
-                all_data = None
+                self.send_arp_request(destination_ip, interface)
+                self.pending_queue.put({'destination_ip': destination_ip, 'message': message})
+        else:
+            print(f"No route found for {destination_ip}. Packet dropped.")
 
-    except FileNotFoundError:
-        # If the file is not found, return None
-        all_data = None
+    def get_routing_interface(self, destination_ip):
+        for entry in self.routing_table:
+            if (self.interface_info.ip_address(destination_ip) & self.interface_info.ip_network(entry['destination'], strict=False).network) == self.ip_network(entry['destination'], strict=False).network:
+                return entry['interface']
+        return None
 
-    return all_data
+    def main_loop(self):
+        threading.Thread(target=self.send_arp_requests).start()
+
+        while True:
+            # Simulate user input
+            user_input = input("Enter message: ")
+            destination_ip = input("Enter destination IP: ")
+            self.forward_packet(destination_ip, user_input)
+
+            # Simulate receiving frames
+            time.sleep(1)
+            self.receive_frame()
+
+            received_frame = self.receive_frame()
+
+            if received_frame:
+                frame_type = self.get_frame_type(received_frame)
+
+                if frame_type == "IP":
+                    # Process IP packet
+                    ip_packet = self.extract_ip_packet(received_frame)
+                    self.process_ip_packet(ip_packet)
+
+                elif frame_type == "ARP":
+                    # Process ARP packet
+                    arp_packet = self.extract_arp_packet(received_frame)
+                    self.process_arp_packet(arp_packet)
+
+            # Check pending queue for IP packets waiting for ARP resolution
+            if not self.pending_queue.empty():
+                pending_ip_packet = self.pending_queue.get()
+                self.encapsulate_ip_packet(pending_ip_packet['destination_ip'], pending_ip_packet['message'])
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 3, 'Usage: python3 station.py ip_address mac_address'
+    assert len(sys.argv) == 3, 'Usage: python station.py ip_address mac_address'
     ip_address = sys.argv[1]
     mac_address = sys.argv[2]
 
-    station = Station(ip_address, mac_address)
-    station.start()
-    station.close()
+    is_router = input("Is this station a router? (y/n): ").lower() == 'y'
+
+    if is_router:
+        router = Router(ip_address, mac_address)
+        router.connect_to_bridges()
+        router.start()
+        router.close()
+    else:
+        station = Station(ip_address, mac_address)
+        station.start()
+        station.close()
